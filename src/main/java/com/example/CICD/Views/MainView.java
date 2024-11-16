9ca73baf94bd583e64643bd65d324e9d3b61c74f
chand <chandunagasudheer215@gmail.com>
2024-11-16 15:46:34 +0530

    Added logic to run bash scripts

--- src/main/java/com/example/CICD/Views/MainView.java
+++ src/main/java/com/example/CICD/Views/MainView.java
@@ -1,13 +1,33 @@
 package com.example.CICD.Views;
 
+import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
+
+import java.io.IOException;
 
 
 @Route("")
 public class MainView extends VerticalLayout {
     public MainView(){
-        add(new H1("kjahvfbkav"));
+        TextField email=new TextField("EMAIL");
+        TextField git=new TextField("GIT-URL");
+        Button btn=new Button();
+        btn.addClickListener(click -> {
+            try {
+            ProcessBuilder processBuilder = new ProcessBuilder("/home/codespace/Scripts_Storage/AutomationController.ctl");
+            processBuilder.directory(new java.io.File(System.getProperty("/home/codespace/Scripts_Storage"))); // Set working directory
+
+            // Start the process
+
+                Process process = processBuilder.start();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        add(new VerticalLayout(email,git,btn));
     }
 }
